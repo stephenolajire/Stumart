2bb65c6394a0cda20fd73bb47b7fc52393a82828
olajirestephen <horlharmighty2000@gmail.com>
2025-04-09 06:01:28 -0600

    hey

--- backend/Project/settings.py
+++ backend/Project/settings.py
@@ -88,16 +88,16 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": dj_database_url.parse(config("external_url"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    "default": dj_database_url.parse(config("external_url"))
+}
 
 
 # DATABASES = {
