91b634ce84bf912ed69bed9eb79c7317a9dd92d2
olajirestephen <horlharmighty2000@gmail.com>
2025-06-05 03:59:00 -0600

    hello'

--- backend/Project/settings.py
+++ backend/Project/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 
