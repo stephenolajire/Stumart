701706b9dbaad1acceae46cdb6e1ce8364d140f6
olajirestephen <horlharmighty2000@gmail.com>
2026-03-09 13:40:44 +0100

    fxed module eroor

--- backend/Project/settings.py
+++ backend/Project/settings.py
@@ -12,6 +12,8 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import sys
+sys.path.insert(0, os.path.join(BASE_DIR))
 
 
 # Quick-start development settings - unsuitable for production
